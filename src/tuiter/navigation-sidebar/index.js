56312faf228f52ea707f03a48696e51da87f34a0
likelike5115 <likelike5115@gmail.com>
2022-11-18 18:31:23 -0500

    a8

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -14,42 +14,42 @@ const NavigationSidebar = () => {
                 <span> Home</span>
             </Link>
             <Link to="/tuiter/explore" className={`list-group-item ${active === 'explore'?'active':''}`}>
-                <i className="fa-solid fa-hashtag me-2"></i>
+                <i className="bi bi-hash me-2"></i>
                 <span> Explore</span>
             </Link>
             <Link to="/" className="list-group-item">
-                <i className="fa-solid fa-asterisk me-2"></i>
+                <i className="bi bi-asterisk me-2"></i>
                 <span> Labs</span>
             </Link>
 
             <a className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
-                <i className="fa-regular fa-bell me-2"></i>
+                <i className="bi bi-bell-fill me-2"></i>
                 <span> Notifications</span>
             </a>
             <a className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
-                <i className="fa-regular fa-envelope  me-2"></i>
+                <i className="bi bi-envelope-fill me-2"></i>
                 <span> Messages</span>
             </a>
 
             <a className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
-                <i className="fa-regular fa-bookmark me-2"></i>
+                <i className="bi bi-bookmark-fill me-2"></i>
                 <span>Bookmarks</span>
             </a>
             <a className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
-                <i className="fa-regular fa-rectangle-list me-2"></i>
+                <i className="bi bi-list-ul me-2"></i>
                 <span>Lists</span>
             </a>
             <Link to="/tuiter/profile" className={`list-group-item ${active === 'profile'?'active':''}`}>
-                <i className="fa-regular fa-user me-2"></i>
+                <i className="bi bi-person-fill me-2"></i>
                 <span>Profile</span>
             </Link>
             <a className={`list-group-item
                     ${active === 'more'?'active':''}`}>
-                <i className="fa-regular fa-comment-dots me-2"></i>
+                <i className="bi bi-three-dots me-2"></i>
                 <span>More</span>
             </a>
         </div>
